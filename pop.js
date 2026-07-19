17d4253a0992629c02e4bb436a10e34d698ef67b
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-19 12:16:16 +0600

    pop system

---
+++ pop.js
@@ -0,0 +1,9 @@
+const numbers = [12, 45, 145, 25, 78, 89, 94];
+console.log(numbers);
+// numbers.pop();
+const firstOut = numbers.pop();
+console.log(numbers);
+// numbers.pop();
+const secondOut = numbers.pop();
+console.log(numbers);
+console.log(firstOut, secondOut);
