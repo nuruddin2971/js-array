35995721da05a9992ebe821609fd8fb757d4a4d9
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-19 11:32:17 +0600

    array index

---
+++ index.js
@@ -0,0 +1,18 @@
+const players = ["abul", "babul", "cabul", "dabul", "kabul"];
+// console.log(players.length);
+
+const player1 = players[0];
+// console.log(player1)
+
+// console.log(players[3]);
+// console.log(players[players.length]);
+// console.log(players);
+// players[1] = "ebul";
+// console.log(players);
+
+let arr = [5, 10, 15];
+console.log(arr);
+arr[1] = 25;
+console.log(arr);
+let last = arr[arr.length - 1];
+console.log(last);
---
+++ length.js
@@ -0,0 +1,11 @@
+const fruits = [
+  "Mango",
+  "Lychee",
+  "jackFruit",
+  "papaya",
+  "grape",
+  "grapefruit",
+];
+const totalFruits = fruits.length;
+console.log(totalFruits);
+console.log(fruits);
