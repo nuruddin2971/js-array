509ec460bf65c385a5f8cfc30c222ce10ab7d6de
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-19 16:39:13 +0600

    for loop for see all index

---
+++ for.js
@@ -0,0 +1,8 @@
+const numbers = [87, 54, 12, 63, 41, 87, 51];
+
+const length = numbers.length;
+for (let i = 0; i < numbers.length; i++) {
+  console.log("index value", i);
+  const item = numbers[i];
+  console.log("value of the item is: ", item);
+}
