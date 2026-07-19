98aad69ec568b2d7e4c6607e6b3b926948d8f659
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-19 16:33:14 +0600

    for of loop

---
+++ loop.js
@@ -0,0 +1,11 @@
+const numbers = [87, 54, 12, 63, 41];
+
+for (const number of numbers) {
+//   console.log("current item is:", number);
+}
+
+const players = ["jamir", "khamir", "namir", "damir"];
+
+for (const p of players) {
+  console.log(p);
+}
