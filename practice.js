2f57f591563291067ad515e22654244e1b69ad8a
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-20 22:39:21 +0600

    complete task 1 to task 6

--- practice.js
+++ practice.js
@@ -1,4 +1,23 @@
 // Task-1 & Task-2
 const arr = ["ami", "tumi", "she"];
 console.log(arr);
-console.log(arr.length)
+console.log(arr.length);
+
+// Task-3
+const arr0 = arr[0];
+const arr2 = arr[2];
+console.log(arr0);
+console.log(arr2);
+
+// Task-4
+arr[1] = "tui";
+console.log(arr);
+
+// Task-5
+arr.push("tora sobai");
+console.log(arr);
+
+// Task-6
+const firstOut = arr.pop();
+console.log(arr);
+console.log(firstOut);
