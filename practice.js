0e5f7cd1863210af3d67b33a72773a9ca00c77bb
nuruddin2971 <nuruddin2971@gmail.com>
2026-07-20 20:51:43 +0600

    task1 and task2

---
+++ practice.js
@@ -0,0 +1,4 @@
+// Task-1 & Task-2
+const arr = ["ami", "tumi", "she"];
+console.log(arr);
+console.log(arr.length)
--- slice.js
+++ slice.js
@@ -4,6 +4,6 @@ const slice = numbers.slice(2, 6);
 // console.log(slice);
 // console.log(numbers);
 
-const parts = numbers.splice(2, 2, 99, 888);
+const parts = numbers.splice(2, 2, 99, 888); 
 console.log(parts);
 console.log(numbers);
